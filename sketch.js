8c6a3ee3d80dbfe18ce64391a9b0d339631a75cf
Wei920204 <131839839+Wei920204@users.noreply.github.com>
2025-04-28 10:53:42 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,11 +18,17 @@ function setup() {
 function draw() {
   background('#caf0f8');
   
+  // 水平翻轉畫布
+  push();
+  translate(width, 0); // 將原點移到畫布右上角
+  scale(-1, 1); // 水平翻轉畫布
+  
   // 顯示攝影機畫面
   image(capture, (width - capture.width) / 2, (height - capture.height) / 2);
   
   // 顯示 overlayGraphics 在攝影機畫面上方
   image(overlayGraphics, (width - capture.width) / 2, (height - capture.height) / 2);
+  pop();
 }
 
 function windowResized() {
